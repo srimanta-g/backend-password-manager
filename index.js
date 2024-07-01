d9c3c9e7379c7f70007c393ee7526754284c1db3
srimanta-g <srimantaganguly02@gmail.com>
2024-07-01 18:52:44 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Server started at ${PORT}`);
+});
