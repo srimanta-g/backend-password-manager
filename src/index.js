0c8309f5d18d3e58a4d666f11114f4d995ccd952
srimanta-g <srimantaganguly02@gmail.com>
2024-09-12 14:57:38 +0530

    Modified the cors properties.

--- src/index.js
+++ src/index.js
@@ -15,8 +15,31 @@ const DATABASE_CONNECTION_URL = process.env.DATABASE_CONNECTION_URL;
 establishConnectionToDatabase(DATABASE_CONNECTION_URL);
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
+app.use(function (req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+
+	// Request methods you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+
+	// Request headers you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-Requested-With,content-type"
+	);
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader("Access-Control-Allow-Credentials", true);
+
+	// Pass to next layer of middleware
+	next();
+});
 app.use(userRouter);
 
 app.listen(PORT, () => {
